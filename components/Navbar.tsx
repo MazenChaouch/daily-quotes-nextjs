32f9f911f6a37527bfe70d4bf6ed3bcbea61ad0a
Mazen Chaouch <m.chaouch007@gmail.com>
2025-05-14 13:46:15 +0100

    Add environment name display to Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export const Navbar = () => {
   return (
     <div className="w-full h-16 flex justify-center items-center bg-teal-400 shadow-xl gap-4">
+      <p>{process.env.name}</p>
       <Link
         href={"/"}
         className="text-white text-lg font-semibold hover:underline transition duration-300"
